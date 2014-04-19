86ea52adc9e09433d647b347d7b6864dbb62a98d
mw <Manoj@Admins-MacBook-Pro.local>
2014-04-19 18:39:50 -0400

    Initial Commit

---
+++ iUtilities/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iUtilities
+//
+//  Created by Manoj on 19/04/14.
+//  Copyright (c) 2014 Manoj Wagh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iUtilities/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iUtilities
+//
+//  Created by Manoj on 19/04/14.
+//  Copyright (c) 2014 Manoj Wagh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
